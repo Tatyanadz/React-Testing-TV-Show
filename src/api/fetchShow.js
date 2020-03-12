333aced2d3d5ee2a284400801eaed91f6e971647
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-03-11 22:17:44 -0400

    MVP complete

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
-export const fetchShow = () => {
+const fetchShow = () => {
     return axios
-      .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-      .then(res => {
-        setShow(res.data);
-        setSeasons(formatSeasons(res.data._embedded.episodes));
-      });
-}
+        .get(
+            "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+        )
+        .then(res => {
+            console.log("API request", res);
+            return res
+        })
+        .catch(err => {
+            console.log("Error fetching data from API", err.message);
+            return err
+        })
+};
+
+export default fetchShow
